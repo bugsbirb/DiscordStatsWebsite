b3b358fb5428902cec2027c5c562a006c58ba802
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-01-24 21:50:56 +0000

    Update page.tsx

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -45,7 +45,7 @@ export default function UpdateStatsDocs() {
         </CardHeader>
         <CardContent>
           <pre className="bg-muted p-4 rounded-md overflow-x-auto">
-            <code>POST http:///Updatestats</code>
+            <code>POST http://stats-pearl.vercel.app/Updatestats</code>
           </pre>
         </CardContent>
       </Card>
